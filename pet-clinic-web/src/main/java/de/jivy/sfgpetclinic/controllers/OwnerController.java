8b80928be8c2f7ce0430998ed4e5274ed1e3b1b7
Oliver Sharif <oliver@sharif-m.de>
2020-03-23 18:51:55 +0100

    Added Owners Page and Controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/de/jivy/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package de.jivy.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+    @RequestMapping({"/owners", "/owners/index", "owners.index.html"})
+    String getText(){
+        return "owners/index";
+    }
+}
